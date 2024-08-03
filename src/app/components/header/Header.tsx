e2fe89045460555db707eea679dadcc4d1c7fbdd
taynara veloso <taynarasantos2.3@hotmail.com>
2024-08-03 19:40:06 -0300

    :star: header navigation

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -4,44 +4,45 @@ import Github from '@/assets/logoGithub.svg'
 
 export function Header() {
   return (
-    <header className="flex items-center justify-between max-w-5xl w-full m-auto p-5">
-      <Image
-      src={Logo}
-      width={286 / 2}
-      alt="Logo da Rocketseat"
-      />
-      <nav className='flex gap-4'>
-
-        <a href="#" aria-label="Para saber mais sobre acessibilidade acesse o site da W3C">
+    <header className="flex shadow-shape h-20">
+      <div className="flex items-center justify-between m-auto max-w-5xl w-full px-4">
+        <Image
+        src={Logo}
+        width={286 / 2}
+        alt="Blog da Rocketseat"
+        />
+        <nav className='flex h-20 items-end content-end flex-wrap gap-4 text-zinc-50'>
+          <a href="#" aria-label="Para saber mais sobre acessibilidade acesse o site da W3C">
+            <button type='button' 
+              aria-label="Página inicial" 
+              className='bg-[#8257e5] rounded-t-md pt-4 px-6'>
+                Home
+            </button>
+          </a>
+          <a href="https://www.w3.org/WAI/tutorials/" aria-label="Para saber mais sobre acessibilidade acesse o site da W3C">
           <button type='button' 
-            aria-label="Página inicial" 
-            className='text-slate-300 bg-[#8257e5] rounded-t-md pt-4 px-6'>
-              Home
-          </button>
-        </a>
-        <a href="https://www.w3.org/WAI/tutorials/" aria-label="Para saber mais sobre acessibilidade acesse o site da W3C">
-        <button type='button' 
-            aria-label="Página inicial" 
-            className='text-slate-300 bg-[#8257e5]  rounded-t-md pt-4 px-6'>
-              Documentation
-          </button>
-        </a>
-        <a href="https://www.w3.org/WAI/tutorials/" aria-label="Para saber mais sobre acessibilidade acesse o site da W3C">
-          <button type='button' 
-            aria-label="Página inicial" 
-            className='text-slate-300 bg-[#8257e5]  rounded-t-md pt-4 px-6'>
-              Guides
-          </button>
-        </a>
-      </nav>
-      <div>
-        <a href="https://github.com/Taynara-Veloso/accessibility_WEB_Fundamentals" aria-label="Para saber mais acesse o GitHub da aplicação">
-          <Image
-            src={Github}
-            width={35}
-            alt=""
-          />
-        </a>
+              aria-label="Página inicial" 
+              className='bg-[#8257e5]  rounded-t-md pt-4 px-6'>
+                Documentation
+            </button>
+          </a>
+          <a href="https://www.w3.org/WAI/tutorials/" aria-label="Acesse os tutoriais de acessibilidade">
+            <button type='button' 
+              aria-label="Página inicial" 
+              className='bg-[#8257e5]  rounded-t-md pt-4 px-6'>
+                Guides
+            </button>
+          </a>
+        </nav>
+        <div>
+          <a href="https://github.com/Taynara-Veloso/accessibility_WEB_Fundamentals" aria-label="Para saber mais acesse o GitHub da aplicação">
+            <Image
+              src={Github}
+              width={35}
+              alt=""
+            />
+          </a>
+        </div>
       </div>
     </header>
   );
