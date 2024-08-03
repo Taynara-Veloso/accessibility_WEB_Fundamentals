19ca506d6559b51417aa9f3e38918ae7d62d9534
taynara veloso <taynarasantos2.3@hotmail.com>
2024-08-03 19:40:53 -0300

    :star: footer

---
+++ src/app/components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="w-screen h-40 bg-[#8257e5] text-slate-100 flex justify-center items-center">
+      Projeto A11y
+    </footer>
+  )
+}
