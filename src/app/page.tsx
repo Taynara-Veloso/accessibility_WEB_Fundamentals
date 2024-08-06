de9803b00da9491c2c3a78255bde323f577a1197
taynara veloso <taynarasantos2.3@hotmail.com>
2024-08-06 02:46:37 -0300

    :star: Home - delete header and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import { Header } from "./components/header/Header";
 import { Main } from "./components/main/Main";
-import { Footer } from "./components/footer/Footer";
 
 export default function Home() {
   return (
     <>
-      <Header/>
       <Main />
-      <Footer />
     </>
   );
 }
